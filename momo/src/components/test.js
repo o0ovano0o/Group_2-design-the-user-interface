b43851bab907cd36b3f57fbf2b873e36bf325d4e
Nguyen Truong Giang <nguyentruonggiangptdq@gmail.com>
2019-09-27 22:32:55 +0700

    initproject

---
+++ momo/src/components/test.js
@@ -0,0 +1,23 @@
+import React, { Component } from 'react';
+import {
+   AppRegistry,
+   StyleSheet,
+   Platform,
+   TextInput,
+   Text,
+   TouchableOpacity,
+   FlatList,
+   View
+} from 'react-native';
+export default class LoginComponent extends Component {
+   
+   render() {
+
+
+   return(  <Text> Đại đạo vô ngân chân khí</Text>)
+     
+
+
+
+   }
+}
